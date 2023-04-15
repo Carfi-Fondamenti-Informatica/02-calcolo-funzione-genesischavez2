1da4961a2450bde4d609581f8908b0dbba67b614
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-04-15 19:23:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+   float a=0,b=0,x=0,y=0;
+   cin >>a>>b>>x>>y;
+   float z;
+   z = a*x – b*y, x<0 and y>0;
+   z = a*x2 – b*y, x>= 0 and y<=0;
+   z = a*x + b*y2;
+   cout << z;  
+ 
    return 0;
 }
