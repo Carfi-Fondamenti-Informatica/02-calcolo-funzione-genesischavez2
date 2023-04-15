03c6d888cd827a58a110199cba7495021a08ec36
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-04-15 19:47:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,15 @@ int main() {
    cin >> a >> b >> x >> y;
    float z=0;
    if((x<0) and (y>0)) {
-      z = (a*x – b*y);
-      cout<<z;
-      } 
+      z = (a*x–b*y);
+      cout<< z;
+   } 
    else if((x>=0) and (y<=0)){
-      z = (a*x*x – b*y);
-      cout<<z;
-   }else  {  
-   z = (a*x + b*y*y);
+      z = (a*x*x–b*y);
+      cout<< z;
+   }
+   else {  
+   z = (a*x+b*y*y);
    cout << z;  
  }
    return 0;
