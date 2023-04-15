8bde3f2ade2c31205ffae020f5d906018a1f33a1
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-04-15 19:52:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-   float a=0,b=0,x=0,y=0;
+   float a=0,b=0,x=0,y=0,z=0;
    cin >> a >> b >> x >> y;
-   float z=0;
+    
    if((x<0) and (y>0)) {
       z = a*x–b*y;
       cout<< z;
